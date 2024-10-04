75e7d4ddc87f6072ea388acf49acbdea5810a322
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-04 21:04:29 +0500

    added user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+
+const userSchema=new mongoose.Schema({
+  username:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  }
+  ,password:{
+    type:String,
+    required:true
+  }
+  ,phoneNumber:{
+    type:Number
+  },
+  address:[
+    {
+      country:{
+        type:String,
+        required:true
+      }
+      ,city:{
+        type:String,
+        required:true
+      }
+      ,homeAddress:{
+        type:String,
+        required:true
+      }
+      ,postalCode:{
+        type:String,
+        required:true
+      }
+    }
+  ],
+  role:{
+    type:String,
+    default:"user"
+  },
+  avatar:{
+    public_id:{
+      type:String,
+      required:true
+    },
+    url:{
+      type:String,
+      required:true
+    }
+  }
+
+},{timestamps:true});
+
+export const User=mongoose.model("User",userSchema);
