d49c7e84e3a07e563dd0695cbb327308fc02593f
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-05 21:20:18 +0500

    created model for event entity

---
+++ backend/models/event.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+
+
+const eventSchema=new mongoose.Schema({
+ name:{
+  type:String,
+  required:true
+ },
+ description:{
+  type:String,
+  required:true
+ },
+ category:{
+  type:String,
+  required:true
+ },
+ start_date:{
+  type:Date,
+  required:true
+ },
+ end_date:{
+   type: Date,
+   required: true
+ },
+ status:{
+  type:String,
+  default:"Running"
+ },
+ tags:{
+  type:String
+ },
+ orignalPrice:{
+   type:Number,
+   required:true
+ },
+ discountPrice:{
+  type:Number,
+  required:true
+ },
+ stock:{
+  type:Number,
+  required:true
+ },
+ images:[
+  {
+    public_id:{
+      type:String
+    },
+    url:{
+      type:String
+    }
+  }
+ ]
+},{timestamps:true});
+
+
+
+export const Event=mongoose.model("Event",eventSchema);
