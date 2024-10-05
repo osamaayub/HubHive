3a6ef1d67b491ec19bbcaff5c2ee3c240ef0dbb9
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-05 17:18:36 +0500

    created model for orders

---
+++ backend/models/order.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+
+const orderSchema=new mongoose.Schema({
+  cart:{
+    type:Array,
+    required:true
+  },
+  shippingAddress:{
+    type:String,
+    required:true
+  },
+  status:{
+    type:String,
+    default:"Processing"
+  },
+  user:{
+    type:Object,
+    required:true
+  },
+  totalPrice:{
+    type:Number,
+    required:true
+  },
+  paymentInfo:{
+    id:{
+      type:String,
+      required:true
+    },
+    type:{
+      type:String
+    },
+    status:{
+      type:String
+    }
+  },
+  paidAt:{
+    type:Date,
+    required:true
+  },
+  deliveredAt:{
+    type:Date
+  }
+},{timestamps:true});
+
+export const Order=mongoose.model("model",orderSchema);
