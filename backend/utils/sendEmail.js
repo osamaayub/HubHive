94c81957a9be38d9de0494838e6472ecf7f016f4
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-09 01:26:15 +0500

    created functionality for email

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+
+
+export const sendEmail = async (options) => {
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    service: process.env.GMAIL_SERVICE,
+    auth: {
+      user: process.env.USER_EMAIL,
+      pass: process.env.USER_PASSWORD
+    }
+  });
+  const mailOptions = {
+    from: process.env.USER_EMAIL,
+    to: options.email,
+    subject: options.subject,
+    text:options.text
+  };
+  await transporter.sendMail(mailOptions);
+}
