40cfe72cb2e799c74824a2fd992736a6ec4de41a
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-08 13:22:05 +0500

    utilis functionality enhanced

---
+++ backend/models/shop.model.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+
+const shopSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true,
+    min:1,
+    max:6,
+    select:false
+  },
+  description:{
+    type:String
+  },
+  address:{
+    type:String,
+    required:true
+  },
+  phoneNumber:{
+    type:Number,
+    required:true
+  },
+  role:{
+    type:String,
+    default:"Seller"
+  },
+  avatar:{
+    public_id:{
+      type:String,
+      required:true
+    },
+    url:{
+      type:String
+    }
+  },
+  zipCode:{
+    type:Number,
+    required:true
+  },
+  withDrawalMethod:{
+    type:Object
+  },
+  availableBalance:{
+    type:Number,
+    default:0
+  },
+  transactions:[
+    {
+      amount:{
+        type:Number,
+        required:true
+      },
+      status:{
+        type:String,
+        default:"Processing"
+      }
+    }
+  ]
+},{timestamps:true});
+
+
+
+export const Shop=mongoose.model("Shop",shopSchema);
---
+++ backend/utils/index.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+
+
+
+export const hashPassword = async (password) => {
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(password, salt);
+}
+
+export const comparePassword = async (password, hashPassword) => {
+  return await bcrypt.compare(password, hashPassword);
+
+}
+
+export const createToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  })
+}
+export const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+}
