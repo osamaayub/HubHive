ea68544a76c3676c16fe7401c416dbd0260d7934
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-10 00:04:20 +0500

    created routed for products

---
+++ backend/routes/product.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { isAdmin, isSeller, isAuthenticated } from "../middlewares/auth";
+
+
+export const productRouter = express.Router();
+
+
+//create a new product
+
+
+productRouter.post("/new-product", createProduct);
+
+//get all the products of the shop
+productRouter.get("/get-all-shop-items/:id", getAllProducts);
+
+
+//delete a product from the shop
+
+productRouter.delete("/delete-shop-item/:id", isSeller, deleteShopItem);
+
+//get all the shop Items
+
+productRouter.get("/get-all-products", getProducts)
+
+//update a review about a product
+
+productRouter.put("/create-new-review", isAuthenticated, updateReview);
+
+
+//products managed by admin
+
+productRouter.get("/admin-all-products", isAuthenticated, isAdmin("Admin"), manageProducts);
+
