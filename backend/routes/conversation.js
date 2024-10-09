7c638151c05972652b3dbdf62aff3d2d5040423c
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-10 01:48:19 +0500

    added route for conversation model

---
+++ backend/routes/conversation.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { isSeller, isAuthenticated } from "../middlewares/auth";
+
+
+
+export const conversationRouter = express.Router();
+
+
+
+//create a new conversation
+
+conversationRouter.post("/create-new-conversation", createNewConversation);
+
+//get all conversation for seller
+
+conversationRouter.get("/get-seller-conversation/:id", isSeller, getSellerConversations);
+
+//get all conversation for user 
+
+conversationRouter.get("/get-user-conversation/:id", isAuthenticated, getUserConversation);
+
+//update the conversation
+
+conversationRouter.put("/update-conversation/:id", updateConversation);
