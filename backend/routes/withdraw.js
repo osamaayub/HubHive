80b91649e82f5390629c3317771ec79ae1d747c7
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-10 00:35:01 +0500

    route created for withdraw model

---
+++ backend/routes/withdraw.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { isSeller, isAdmin, isAuthenticated } from "../middlewares/auth";
+
+const WithdrawRouter = express.Router();
+
+
+
+//create a new withdraw request for seller only
+
+
+WithdrawRouter.post("/create-withdraw-request", isSeller, createWithdrawRequest);
+
+
+//get all  of the  withdraw request for admins
+
+WithdrawRouter.get('/get-all-payments', isAuthenticated, isAdmin("Admin"), getAllPayments);
+
+//update all of the withdraw request
+
+WithdrawRouter.put("/update-withdraw-request/:id", updateWithdrawRequest);
