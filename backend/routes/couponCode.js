bc86e768612f49637dea80b9b64a88afbf9cf12d
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-10 01:34:54 +0500

    created route for couponcode model

---
+++ backend/routes/couponCode.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { isSeller } from "../middlewares/auth";
+
+
+export const couponCodeRouter = express.Router();
+
+//create a new coupon
+
+couponCodeRouter.post("/create-new-coupon", isSeller, createNewCoupon);
+
+//get  coupon code
+
+couponCodeRouter.get("/get-coupon-code/:id", isSeller, getCouponCode);
+
+
+//delete coupon code
+
+couponCodeRouter.delete("/delete-coupon-code/:id", isSeller, deleteCouponCode);
+
+//get coupon code by name
+
+couponCodeRouter.get("/get-coupon-code/:name", isSeller, getCouponCodebyName);
