694a80688d79941386a528d8479b8a8b4c0fefc6
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-10 01:13:32 +0500

    created route for event model

---
+++ backend/routes/event.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { isAdmin, isSeller, isAuthenticated } from "../middlewares/auth";
+
+
+export const eventRouter = express.Router();
+
+
+//create a new event
+
+eventRouter.post("/create-new-event", createNewEvent);
+
+
+//get all Events
+
+eventRouter.get("/get-all-events", getAllEvents);
+
+//get event of the shop
+
+eventRouter.get("/get-all-events/:id", getEvents);
+
+//delete a event of an shop
+
+eventRouter.delete("/delete-event/:id", deleteEvent);
+
+//events managed by admin
+
+eventRouter.get("/admin-all-events", isAuthenticated, isAdmin("Admin"), getAdminEvents)
