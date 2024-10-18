0cace33dbec47daeb2c53c28dec9145ae5573195
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-19 02:50:28 +0500

    added version for the routes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,18 @@ import "dotenv/config";
 import cors from "cors";
 import ConnectDB from "./config/database.js";
 import { v2 as cloudinary } from "cloudinary";
+import {
+  conversationRouter,
+  couponCodeRouter,
+  eventRouter,
+  messageRouter,
+  orderRouter,
+  productRouter,
+  userRouter,
+  withdrawRouter
+} from "./routes";
+import cookieParser from "cookie-parser";
+import { shopRouter } from "./routes/shop.js";
 
 
 
@@ -17,8 +29,20 @@ cloudinary.config({
 })
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors());
+
+
+app.use("/api/v2/conversation", conversationRouter);
+app.use("/api/v2/couponCode", couponCodeRouter);
+app.use("/api/v2/event", eventRouter);
+app.use("/api/v2/message", messageRouter);
+app.use("/api/v2/orders", orderRouter);
+app.use("/api/v2/products", productRouter);
+app.use("/api/v2/shop", shopRouter);
+app.use("/api/v2/users", userRouter);
+app.use("/api/v2/withdraw", withdrawRouter);
 
 ConnectDB();
 
