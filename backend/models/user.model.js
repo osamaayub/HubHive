460d1f6053c8200310e09c06cd2655fc64a8db77
osamayub <usama.ayub1@ucp.edu.pk>
2024-10-19 12:31:51 +0500

    scheme field updated

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -56,7 +56,7 @@ const userSchema = new mongoose.Schema({
     }
   },
   resetPasswordToken: String,
-  resetPasswordTime: String
+  resetPasswordTime: Date
 
 }, { timestamps: true });
 userSchema.pre("save", async (next) => {
